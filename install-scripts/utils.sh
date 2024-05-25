fdb7e4c8bd3200511672e71fb6dee386caadf619
Mr.L <noone@gmail.com>
2024-05-25 20:31:07 +0300

    Added echo_success and COPY_FILES_DIR to utils.sh.
    Added a constant for the files dir, and a echo_success function.

--- install-scripts/utils.sh
+++ install-scripts/utils.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+######################
+## useful constants ##
+######################
+
+COPY_FILES_DIR="../files"
+
+
+###################
+## useful prints ##
+###################
+
 ANSI_RED='\033[0;31m'
 ANSI_YELLOW='\033[1;33m'
 ANSI_CYAN='\033[0;34m'
+ANSI_GREEN='\033[0;32m'
 
 ANSI_RESET='\033[0m'
+
+echo_success ()
+{
+	echo -e "${ANSI_GREEN}success${ANSI_RESET}: $@"
+}
 
 echo_info ()
 {

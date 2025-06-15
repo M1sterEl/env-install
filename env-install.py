8777b052f806868be5054c3bb5cecebb68edf9cc
mikel rabukhin <mr.l@Mac.lan>
2025-06-15 20:14:01 +0300

    Added the nvim_subparser to the main cli file.

--- env-install.py
+++ env-install.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.10
 """
 The file for the main command for installing the wanted environment.
 """
 import argparse
+from install_scripts.install_setup_nvim import nvim_parser
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
+    main_parser = argparse.ArgumentParser(
                     prog="env-install",
                     )
 
-    subparsers = parser.add_subparsers(help="what to install")
+    subparsers = main_parser.add_subparsers(help="what to install")
     subparsers.required = True
 
+
+    nvim_parser(subparsers)
+
+    args = main_parser.parse_args()
+    args.func(args)

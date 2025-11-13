4cc86cbd41afa17e8c8a98d11bb9748c30d65b2a
mikel rabukhin <mr.l@desktop.lan>
2025-11-13 22:55:26 +0200

    EI-42: Modified main script
    Changed the install subcommand to setup.
    Added a build subcommand, the subcommand is a WIP.
    Added argument to require the user to specify on which os the
    env-install is running.

--- env-install.py
+++ env-install.py
@@ -3,7 +3,11 @@
 The file for the main command for installing the wanted environment.
 """
 import argparse
-from install_scripts import nvim_parser, git_config_parser
+
+from install_scripts import nvim_parser, git_parser
+
+from install_scripts import constants as global_constants
+
 
 if __name__ == "__main__":
 
@@ -11,13 +15,31 @@ if __name__ == "__main__":
                     prog="env-install",
                     )
 
-    subparsers = main_parser.add_subparsers(help="what to install")
-    subparsers.required = True
+    action_subparsers = main_parser.add_subparsers(help="what action to do")
+    action_subparsers.required = True
 
+    setup_parser = action_subparsers.add_parser("setup",
+                                                  help="setup parts/the whole of the environment")
 
-    nvim_parser(subparsers)
+    setup_parser.add_argument("target_os",
+                                choices=global_constants.SUPPORTED_TARGET_OSS,
+                                help="target system the setup is run on")
 
-    git_config_parser(subparsers)
+    setup_target_subparser = setup_parser.add_subparsers(help="what to setup")
+
+    nvim_parser(setup_target_subparser)
+
+    git_parser(setup_target_subparser)
+
+    build_parser = action_subparsers.add_parser("build",
+                                                  help="build supported outputs")
+
+    build_target_subparser = build_parser.add_subparsers(help="what to build", dest="build")
+
 
     args = main_parser.parse_args()
+
+    if hasattr(args, "build"):
+        raise NotImplementedError("The build command is not implemented yet")
+
     args.func(args)

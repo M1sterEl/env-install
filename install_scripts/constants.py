16736983a072d1bf64b8ce0803073a44fc997195
mikel rabukhin <mr.l@desktop.lan>
2025-11-13 22:55:26 +0200

    EI-42: Modified main script
    Changed the install subcommand to setup.
    Added a build subcommand, the subcommand is a WIP.
    Added argument to require the user to specify on which os the
    env-install is running.

--- install_scripts/constants.py
+++ install_scripts/constants.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from  os import getenv
+from os import getenv
 import pathlib
 
+# Nvim install.
+NVIM_VERSION="v0.10.0"
+
+# Host paths.
 HOME = getenv("HOME")
-
 DEFAULT_CONFIG_DIR = f"{HOME}/.config/"
-
 TOP_PROJECT_DIR = pathlib.Path(__file__).parent.parent
+DEFAULT_FILES_DIR = pathlib.Path(f"{TOP_PROJECT_DIR}/files")
 
-DEFUALT_FILES_DIR = pathlib.Path(f"{TOP_PROJECT_DIR}/files")
+# Target info.
+SUPPORTED_TARGET_OSS = ["debian", "mac"]

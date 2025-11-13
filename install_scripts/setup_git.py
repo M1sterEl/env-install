9f1c464048655ecfca65c7ab990173c35b787737
mikel rabukhin <mr.l@desktop.lan>
2025-11-13 22:58:19 +0200

    EI-42: Added lazygit install to the env.
    Added installation of Lazygit to the environment.
    Changed git_config_parser to git_parser.

--- install_scripts/setup_git.py
+++ install_scripts/setup_git.py
@@ -3,15 +3,16 @@
 import argparse
 import shutil
 import pathlib
+import subprocess
 
 import install_scripts.constants as global_constants
-from .utils import print_success, print_info
+from .utils import print_success, print_info, print_success, print_warning
 
-GIT_FILES_DIR = pathlib.Path(f"{global_constants.DEFAULT_CONFIG_DIR}/git")
+GIT_FILES_DIR = pathlib.Path(f"{global_constants.DEFAULT_FILES_DIR}")
 
 
 # We add the args to maintain the format for the rest of the sub commands default functions.
-def copy_custom_gitconfig(args) -> None:
+def copy_custom_gitconfig() -> None:
     """
     Copies a .gitconfig file to the home dir.
     """
@@ -23,11 +24,63 @@ def copy_custom_gitconfig(args) -> None:
     print_success(f"copied .gitconfig file to {global_constants.HOME}")
 
 
-def git_config_parser(subparsers_object: argparse.ArgumentParser.add_subparsers):
+def install_lazygit(target_os: str, offline: bool) -> None:
+    """
+    Installs Lazygit to the environment.
 
-    git_config_parser = subparsers_object.add_parser("gitconfig", help="copies .gitconfig to the {global_constants.HOME}")
+    :param target_os:   the os we are installing lazygit on.
+    :param offline:     whether to assume internet connection or not (true; to assume, false; to not assume).
+    """
 
-    git_config_parser.set_defaults(func=copy_custom_gitconfig)
+    print_info(f"installing lazygit using the method for {target_os}")
+
+    if offline:
+        raise NotImplementedError("offline install of lazygit not supported yet")
+
+    success = True
+
+    try:
+        match target_os:
+            case "mac":
+                print_info(f"{target_os} was defined as the target os, installing using homebrew")
+                subprocess.run(["brew", "install", "lazygit"], capture_output=False)
+
+            case "debian":
+                print_info(f"{target_os} was defined as the target os, installing using apt")
+                subprocess.run(["sudo", "apt", "install", "lazygit"], capture_output=False)
+
+            case _:
+                print_error("unrecognized target os detected, didn't install lazygit")
+                success = False
+    except:
+        success = False
+
+    if success:
+        print_success("installed lazygit to environment")
+
+    else:
+        print_warning("didn't install lazygit to environment")
+
+
+def main(args) -> None:
+    """
+    Copy gitconfig file and install lazyvim based on given os.
+    """
+
+    copy_custom_gitconfig()
+
+    install_lazygit(args.target_os, args.offline)
+
+
+def git_parser(subparsers_object: argparse.ArgumentParser.add_subparsers):
+
+    git_parser = subparsers_object.add_parser("git", help="copies .gitconfig to the {global_constants.HOME}")
+
+    git_parser.add_argument("-o", "--offline",
+                             help="install while assuming no internet connection",
+                             action="store_true")
+
+    git_parser.set_defaults(func=main)
 
 
 if __name__ == "__main__":
